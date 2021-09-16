9b9e3338553707f5729c647b320f193e70c30007
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-16 15:07:25 -0500

    Se crea el proyecto para backend, added imagen dependencias

---
+++ bookstore-backend/src/test/java/pe/popehiflo/bookstorebackend/BookstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.popehiflo.bookstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
