65ae6f8c0a8988384e8dd125ff92960b129882d5
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-16 16:46:51 -0500

    Added anotaciones a la entidades Categoria y Libro

--- bookstore-backend/src/main/java/pe/popehiflo/bookstorebackend/model/Categoria.java
+++ bookstore-backend/src/main/java/pe/popehiflo/bookstorebackend/model/Categoria.java
@@ -1,13 +1,26 @@
 package pe.popehiflo.bookstorebackend.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
+	@OneToMany(mappedBy = "categoria")
 	private List<Libro> libros = new ArrayList<>();
 	
 	public Categoria() {
--- bookstore-backend/src/main/java/pe/popehiflo/bookstorebackend/model/Libro.java
+++ bookstore-backend/src/main/java/pe/popehiflo/bookstorebackend/model/Libro.java
@@ -1,16 +1,33 @@
 package pe.popehiflo.bookstorebackend.model;
 
-public class Libro {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table()
+public class Libro implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nombre_autor;
 	private String texto;
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 	
 	public Libro() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public Libro(Integer id, String titulo, String nombre_autor, String texto, Categoria categoria) {
